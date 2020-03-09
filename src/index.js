b2aadd2ef0c011983f949e1b4dc2df47e138ef83
Nhung <54989905+dothinhung123@users.noreply.github.com>
2020-03-09 15:41:34 +0700

    delete all file

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import App from './components/App'
-import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
-import {Provider} from 'react-redux';
-import reducers from './reducers'
-
-ReactDOM.render(<Provider store ={createStore(reducers)}><App/></Provider>, document.querySelector('#root'))
