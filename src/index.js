12377c9a969db9988880934e6a3f80029a4bfee0
Nhung <54989905+dothinhung123@users.noreply.github.com>
2020-03-10 13:38:21 +0700

    Add all file

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import reducers from './reducers'
-import {Provider} from 'react-redux'
-import {createStore} from 'redux';
-import App from './components/App'
-
-ReactDOM.render(<Provider store={createStore(reducers)}><App></App></Provider>,document.querySelector('#root'))
